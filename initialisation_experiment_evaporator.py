d49842e13f332e0a9ca14d7b13f5213fe851db14
bd65 <bd65@students.waikato.ac.nz>
2025-05-14 16:49:44 +1200

    trying with a bunch of heat duties

--- initialisation_experiment_evaporator.py
+++ initialisation_experiment_evaporator.py
@@ -14,6 +14,8 @@ from property_packages.build_package import build_package
 from direct_steam_injection import Dsi
 from translator import GenericTranslator
 import idaes.logger as idaeslog
+from idaes.core.util.model_serializer import from_json, to_json
+import time
 # New solver interface: http://pyomo.readthedocs.io/en/6.8.0/developer_reference/solvers.html
 from pyomo.contrib.solver.util import assert_optimal_termination, SolutionStatus, TerminationCondition
 from pyomo.contrib.solver.ipopt import Ipopt
@@ -69,6 +71,9 @@ m.fs.flash_phase_separator_to_effect = Arc(
 pyo.TransformationFactory("network.expand_arcs").apply_to(m)
 
 
+
+
+
 # Specify the properties
 m.fs.dsi.inlet.flow_mol.fix(50)
 m.fs.dsi.inlet.temperature.fix(351.15) # 78.0 C
@@ -76,81 +81,76 @@ m.fs.dsi.inlet.pressure.fix(90000) # 90 kPa
 m.fs.dsi.inlet.mole_frac_comp[0, "water"].fix(0.95)
 m.fs.dsi.inlet.mole_frac_comp[0, "milk_solid"].fix(0.05)
 
-
 m.fs.dsi.steam_inlet.pressure.fix(1_000_000) # 10 bar
 m.fs.dsi.properties_steam_in[0].constrain_component(m.fs.dsi.properties_steam_in[0].temperature, 458.15) # 185 C
 m.fs.dsi.outlet.temperature.fix(368.15) # 95 C, this is used to calculate the flowrate of the steam_inlet.
 
 m.fs.flash.valve_opening.fix(1)
-# Add a constraint to fix the outlet pressure of the flash, which should calcualte the valve coefficient.
+# Add a constraint to fix the outlet pressure of the flash, which should calculate the valve coefficient.
 @m.fs.Constraint()
 def flash_pressure_constraint(fs):
     return fs.flash.outlet.pressure[0] == 75_000 # 75 kPa
 m.fs.flash.Cv.unfix() # IDK why this is fixed by default.
 
-
-
 m.fs.flash_phase_separator.split_fraction[0,"outlet_1", "Vap"].fix(0.02)
 m.fs.flash_phase_separator.split_fraction[0,"outlet_1", "Liq"].fix(0.99)
 
 
-m.fs.effect_1.heat_duty.fix(-15_000)
-#m.fs.effect_1.outlet.temperature.fix(363.559) 
-
-def init_unit(unit):
-    print(f"Initializing unit {unit}")
-    unit.initialize(outlvl=idaeslog.DEBUG)
-
-# Use SequentialDecomposition to initialise the model
-seq = SequentialDecomposition()
-# use create_graph to get the order of sequential decomposition, and also to
-# find any units that are not connected to the sequential decomposition
-seq.set_tear_set([]) # No tears required.
-G = seq.create_graph(m)
-order = seq.calculation_order(G)
-seq_blocks = []
-for o in order:
-    seq_blocks.append(o[0])
-print("Order of initialisation:", [blk.name for blk in seq_blocks])
-seq.options["tol"] = 1e-2
-# seq.options["solve_tears"] = False
-res = seq.run(m, init_unit)
-
-# Solve the model
-print("Degrees of freedom:", degrees_of_freedom(m))
-print("Degrees of freedom in dsi:", degrees_of_freedom(m.fs.dsi))
-print("Degrees of freedom in flash:", degrees_of_freedom(m.fs.flash))
-print("Degrees of freedom in flash_phase_separator:", degrees_of_freedom(m.fs.flash_phase_separator))
-
-assert degrees_of_freedom(m) == 0
-opt = Ipopt()
-opt.config.raise_exception_on_nonoptimal_result = False
-status = opt.solve(m, tee=True)
-status.display()
-print(status.iteration_count)
-print(status.timing_info.wall_time)
-print(status.termination_condition)
-print(status.solution_status)
-
-print(m.fs.effect_1.outlet.temperature[0].value)
-print(m.fs.effect_1.outlet.mole_frac_comp[0,"milk_solid"].value)
-print(m.fs.effect_1.outlet.mole_frac_comp[0,"water"].value)
+default_values = to_json(m,return_dict=True)
 
 
-assert_optimal_termination(status)
+HEAT_DUTY_VALUES = [0,1000,2000, 4000, 8000, 12000, 16000, 20000]
+time_results = []
+iteration_results = []
+solve_status = []
 
-# This solves with heat duty fixed fine, but if you uncommented the line above that 
-# fixes the effect_1 outlet temperature, and comment out the heat_duty, it would not solve.
-# However, we now show that once you have solved with the heat_duty fixed, you can then
-# unfix the heat_duty and fix the temperature and it solves fine.
 
-m.fs.effect_1.heat_duty.unfix()
-m.fs.effect_1.outlet.temperature.fix(360.559) 
+for heat_duty in HEAT_DUTY_VALUES:
+    from_json(m,sd=default_values)
+    start = time.time()
+    
+    
+    m.fs.effect_1.heat_duty.fix(heat_duty)
+    #m.fs.effect_1.outlet.temperature.fix(363.559) 
 
-status2 = opt.solve(m, tee=True)
-print("attempt 2")
-assert_optimal_termination(status2)
-print("Iterations of first solve:", status.iteration_count)
-print("Iterations of second solve:", status2.iteration_count)
-print("Wall time of first solve:", status.timing_info.wall_time)
-print("Wall time of second solve:", status2.timing_info.wall_time)
+    def init_unit(unit):
+        print(f"Initializing unit {unit}")
+        unit.initialize()#outlvl=idaeslog.DEBUG
+
+    # Use SequentialDecomposition to initialise the model
+    seq = SequentialDecomposition()
+    # use create_graph to get the order of sequential decomposition, and also to
+    # find any units that are not connected to the sequential decomposition
+    seq.set_tear_set([]) # No tears required.
+    G = seq.create_graph(m)
+    order = seq.calculation_order(G)
+    seq_blocks = []
+    for o in order:
+        seq_blocks.append(o[0])
+    print("Order of initialisation:", [blk.name for blk in seq_blocks])
+    seq.options["tol"] = 1e-2
+    # seq.options["solve_tears"] = False
+    res = seq.run(m, init_unit)
+
+    # Solve the model
+    print("Degrees of freedom:", degrees_of_freedom(m))
+    print("Degrees of freedom in dsi:", degrees_of_freedom(m.fs.dsi))
+    print("Degrees of freedom in flash:", degrees_of_freedom(m.fs.flash))
+    print("Degrees of freedom in flash_phase_separator:", degrees_of_freedom(m.fs.flash_phase_separator))
+
+    assert degrees_of_freedom(m) == 0
+    opt = Ipopt()
+    opt.config.raise_exception_on_nonoptimal_result = False
+    status = opt.solve(m, tee=False)
+    print(status.iteration_count)
+    print(status.timing_info.wall_time)
+    print(status.termination_condition)
+    print(status.solution_status)
+
+    end = time.time()
+    time_results.append(end - start)
+    iteration_results.append(status.iteration_count)
+    solve_status.append(status.solution_status)   
+
+for results in zip(HEAT_DUTY_VALUES, time_results, iteration_results, solve_status):
+    print(results)
